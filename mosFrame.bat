20e664645e27ea13e1b86b8a25936d131acbd076
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2021-08-05 11:16:57 +0430

    add compiled source

--- mosFrame.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-@echo " This is an educational legacy material from the old days of the net"
-@echo " All credits are belong to ubuffalo.edu" 
-@echo " Prepared by: M. B. Ghaznavi-Ghoushchi" 
-@echo " Developed by: Seyyed Mahdiyar Zerehpoush"
-
-"%JAVA_HOME%\bin\appletviewer.exe" index.html
-pause
